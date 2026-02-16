2dfe52e8a17400d22f19af7903bb96e4a2f2ef16
Florian Fischer <flottokarotto@gmail.com>
2026-02-16 23:25:53 +0100

    Move PV charger example to dedicated branch nucleo_pv_charger

--- examples/embedded/nucleo_h753_pv_charger/src/time_exports.ads
+++
@@ -1,9 +0,0 @@
---  Time_Exports - Export tick function to C
---  Copyright (c) 2026 Florian Fischer
---  SPDX-License-Identifier: MIT
-
-package Time_Exports is
-
-   pragma Elaborate_Body;
-
-end Time_Exports;
